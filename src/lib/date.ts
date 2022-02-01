697fee5cf13859b8740269dc5b3849eb476e163a
Nick Douglas <douglas.extern@schuettflix.de>
2022-02-01 22:21:58 +0000

    Updated dates

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -2,5 +2,8 @@ import format from 'date-fns/format'
 import parseISO from 'date-fns/parseISO'
 
 export const formatDate = (isoString: string) => {
+  if (!isoString) {
+    return undefined
+  }
   return format(parseISO(isoString), 'do MMM yyyy')
 }
